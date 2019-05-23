1ddc856ec8084073892f870f288fbe03d72cda6a
Mike Keen <mkeen.atl@gmail.com>
2019-05-23 16:37:01 -0400

    Salt/Salt minion config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,19 @@
 # SSH Tokens
-variable "mike_key" {
-  type = "string"
-  default = "2e:05:a5:d1:0f:9e:d8:4e:06:af:20:e7:d4:8b:7b:96"
-}
 
-variable "salt_master_key" {
-  type = "string"
-  default = "49:a2:23:92:fd:ec:8d:be:27:b8:b7:a6:a3:4a:77:7e"
-}
 
 #variable "jenkins_key" {
 #  type = "string"
 #  default = "7d:19:b0:09:11:5f:81:b8:dc:08:9c:f4:3e:29:74:f1"
 #}
+
+data "local_file" "mike_key" {
+  filename = "/home/guest/.ssh/id_rsa.pub"
+}
+
+resource "digitalocean_ssh_key" "mike_keen_key" {
+  name = "Mike Keen Key"
+  public_key = "${data.local_file.mike_key.content}"
+}
 
 variable "digitalocean_api_token" {}
 
@@ -22,29 +23,21 @@ provider "digitalocean" {
 }
 
 module "Salt_Master" {
-  source = "modules/salt"
-  master = true
+  source = "modules/salt-master"
   name = "saltm"
-  key = ["${var.mike_key}"]
+  keys = ["${digitalocean_ssh_key.mike_keen_key.fingerprint}"]
   salt_minion_roles = ["  - master"]
   domain_id = "terragon.us"
 }
 
-#module "CouchDBMasterNode" {
-#  source = "modules/salt"
-#  minion = true
-#  name = "couchdb-a"
-#  tld = "terragon.us"
-  #image = "ubuntu-16-04-x64"
-#  size = "512mb"
-#  static_ip = true
-#  is_ubuntu = false
-#  is_bsd = true
-#  create_tld = false
-#  domain_id = "terragon.us"
-#  key = ["${var.mike_key}"]
-#  salt_master_fqdn = "${module.Salt_Master.salt_master_fqdn}"
-#  salt_minion_roles = ["  - couchdb", "  - minion", "  - couchdbmaster"]
-#  salt_master_private_ip_address = "${module.Salt_Master.salt_master_private_ip_address}"
-#  salt_master_public_ip_address = "${module.Salt_Master.salt_master_public_ip_address}"
-#}
+module "CouchDBMasterNode" {
+  source = "modules/salt-minion"
+  name = "couchdb-a"
+  size = "512mb"
+  domain_id = "terragon.us"
+  keys = ["${digitalocean_ssh_key.mike_keen_key.fingerprint}"]
+  salt_master_fqdn = "${module.Salt_Master.salt_master_fqdn}"
+  salt_minion_roles = ["  - couchdb", "  - minion", "  - couchdbmaster"]
+  salt_master_private_ip_address = "${module.Salt_Master.salt_master_private_ip_address}"
+  salt_master_public_ip_address = "${module.Salt_Master.salt_master_public_ip_address}"
+}
