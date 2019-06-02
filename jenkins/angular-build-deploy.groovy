d43e42075815ec1545e41b1c0f1f85b1ccd3ef6b
Mike Keen <mkeen.atl@gmail.com>
2019-06-02 12:52:40 -0400

    ??

--- jenkins/angular-build-deploy.groovy
+++ jenkins/angular-build-deploy.groovy
@@ -11,35 +11,35 @@ node {
             }
 
              stage('NPM Build') {
-                        def exitCodeBuild = sh script: 'npm build', returnStatus: true
-                        
-                        if(exitCodeBuild != '0') {
-                            currentBuild.result = 'FAILURE'
-                        } else {
-                            withCredentials([
-                                string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
-                                string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
-                            ]) {
-                                stage('Publish') {
-                                    def exitCodeSpacesUpload = sh script: "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist s3://www-terragon-us/", returnStatus: true
-                                    if(exitCodeSpacesUpload != '0') {
-                                        // Need to roll back
-                                        currentBuild.result = 'FAILURE'
-                                    } else {
-                                        currentBuild.result = 'SUCCESS'
-                                    }
-                                    
-                                }
-                                
-                            }
-                            
-                        }
-                        
+                def exitCodeBuild = sh script: 'npm build', returnStatus: true
+                
+                if(exitCodeBuild != '0') {
+                    currentBuild.result = 'FAILURE'
+                } 
+                
+            }
+
+            stage('Publish') {
+                withCredentials([
+                    string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
+                    string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
+                ]) {
+                    def exitCodeSpacesUpload = sh script: "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist s3://www-terragon-us/", returnStatus: true
+                    if(exitCodeSpacesUpload != '0') {
+                        // Need to roll back
+                        currentBuild.result = 'FAILURE'
+                    } else {
+                        currentBuild.result = 'SUCCESS'
                     }
                     
-        
+                }
+                
+            }
+            
         }
         
     }
     
+}
+
 }
