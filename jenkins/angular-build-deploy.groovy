c76d9da4164c83d9ee89c31bccebc685629d140f
Mike Keen <mkeen.atl@gmail.com>
2019-06-02 16:45:38 -0400

    this api is something else

--- jenkins/angular-build-deploy.groovy
+++ jenkins/angular-build-deploy.groovy
@@ -1,33 +1,27 @@
 node {
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
-        stages {
-            stage('Checkout') {
-                git url: 'https://github.com/terragon-social-club/infrastructure'     
-            }
-            
-            stage('NPM Install') {
-                sh 'npm install'
-            }
-            
-            stage('NPM Build') {
-                sh 'npm run build'
-            }
-            
-            stage('Publish') {
-                withCredentials([
-                    string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
-                    string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
-                ]) {
-                    sh "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist/terragon s3://www-terragon-us/ --endpoint-url https://nyc3.digitaloceanspaces.com --acl public-read"
-                }
-                
-            }
+        stage('Checkout') {
+            git url: 'https://github.com/terragon-social-club/infrastructure'     
+        }
+
+        stage('NPM Install') {
+            sh 'npm install'
         }
         
-        post { 
-            always { 
+        stage('NPM Build') {
+            sh 'npm run build'
+        }
+        
+        stage('Publish') {
+            withCredentials([
+                string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
+                string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
+            ]) {
+                sh "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist/terragon s3://www-terragon-us/ --endpoint-url https://nyc3.digitaloceanspaces.com --acl public-read"
+
                 cleanWs()
             }
+            
         }
         
     }
