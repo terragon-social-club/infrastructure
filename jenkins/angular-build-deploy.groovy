ad3f35b525e319457bfe4bbf18f8226a47942d7d
Mike Keen <mkeen.atl@gmail.com>
2019-06-02 16:37:23 -0400

    fix this ugly script

--- jenkins/angular-build-deploy.groovy
+++ jenkins/angular-build-deploy.groovy
@@ -1,23 +1,24 @@
 node {
-    stage('Checkout') {
-        git credentialsId: 'mkeen', url: 'https://github.com/terragon-social-club/terragon'
-        wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
-            stage('NPM Install') {
-                script: 'npm install'
-            }
+    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
+    
+        stage('Checkout') {
+            git credentialsId: 'mkeen', url: 'https://github.com/terragon-social-club/terragon'        
+        }
 
-             stage('NPM Build') {
-                script: 'npm run build'
-            }
-
-            stage('Publish') {
-                withCredentials([
-                    string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
-                    string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
-                ]) {
-                    sh "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist/terragon s3://www-terragon-us/ --endpoint-url https://nyc3.digitaloceanspaces.com --acl public-read"
-                }
-                
+        stage('NPM Install') {
+            script: 'npm install'
+        }
+        
+        stage('NPM Build') {
+            script: 'npm run build'
+        }
+        
+        stage('Publish') {
+            withCredentials([
+                string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
+                string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
+            ]) {
+                sh "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist/terragon s3://www-terragon-us/ --endpoint-url https://nyc3.digitaloceanspaces.com --acl public-read"
             }
             
         }
