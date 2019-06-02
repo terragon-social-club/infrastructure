63cbabd637eebb491a858af6b2b0a98672a82c04
Mike Keen <mkeen.atl@gmail.com>
2019-06-02 13:07:47 -0400

    ???

--- jenkins/angular-build-deploy.groovy
+++ jenkins/angular-build-deploy.groovy
@@ -25,12 +25,7 @@ node {
                     string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
                 ]) {
                     def exitCodeSpacesUpload = sh script: "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist/terragon s3://www-terragon-us/ --endpoint-url https://nyc3.digitaloceanspaces.com", returnStatus: true
-                    if(exitCodeSpacesUpload != '0') {
-                        // Need to roll back
-                        currentBuild.result = 'FAILURE'
-                    } else {
-                        currentBuild.result = 'SUCCESS'
-                    }
+                    currentBuild.result = 'SUCCESS'
                     
                 }
                 
