8e60ba2b9d1e7aaf61541dc245f39898e61462c1
Mike Keen <mkeen.atl@gmail.com>
2019-06-02 11:19:35 -0400

    maybe dont need return status

--- jenkins/angular-build-deploy.groovy
+++ jenkins/angular-build-deploy.groovy
@@ -3,12 +3,12 @@ node {
     git credentialsId: 'mkeen', url: 'https://github.com/terragon-social-club/terragon'
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
         stage name: 'NPM Install', concurrency: 1
-        def exitCodeInstall = sh 'npm install', returnStatus: true
+        def exitCodeInstall = sh 'npm install'
         if(exitCodeInstall != '0') {
             currentBuild.result = 'FAILURE'
         } else {
             stage name: 'NPM Build', concurrency: 1
-            def exitCodeBuild = sh 'npm build', returnStatus: true
+            def exitCodeBuild = sh 'npm build'
 
             if(exitCodeBuild != '0') {
                 currentBuild.result = 'FAILURE'
@@ -17,7 +17,7 @@ node {
                     string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
                     string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
                 ]) {
-                    def exitCodeSpacesUpload = sh "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist s3://www-terragon-us/", returnStatus: true
+                    def exitCodeSpacesUpload = sh "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist s3://www-terragon-us/"
                     if(exitCodeSpacesUpload != '0') {
                         // Need to roll back
                         currentBuild.result = 'FAILURE'
