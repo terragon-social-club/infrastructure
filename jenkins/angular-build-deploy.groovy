30dc990495e202d6b66ff7c8b6d0b3edadf530d9
Mike Keen <mkeen.atl@gmail.com>
2019-06-02 16:20:17 -0400

    angular

--- jenkins/angular-build-deploy.groovy
+++ jenkins/angular-build-deploy.groovy
@@ -3,20 +3,11 @@ node {
         git credentialsId: 'mkeen', url: 'https://github.com/terragon-social-club/terragon'
         wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
             stage('NPM Install') {
-                def exitCodeInstall = sh script: 'npm install', returnStatus: true
-                if(exitCodeInstall != '0') {
-                    currentBuild.result = 'FAILURE'
-                }
-                
+                script: 'npm install'
             }
 
              stage('NPM Build') {
-                def exitCodeBuild = sh script: 'npm run build', returnStatus: true
-                
-                if(exitCodeBuild != '0') {
-                    currentBuild.result = 'FAILURE'
-                } 
-                
+                script: 'npm run build'
             }
 
             stage('Publish') {
@@ -24,9 +15,7 @@ node {
                     string(credentialsId: 'digitalocean_spaces_secret_key', variable: 'DO_SP_SC'),
                     string(credentialsId: 'digitalocean_spaces_access_id', variable: 'DO_SP_ID')
                 ]) {
-                    def exitCodeSpacesUpload = sh script: "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist/terragon s3://www-terragon-us/ --endpoint-url https://nyc3.digitaloceanspaces.com --acl public-read", returnStatus: true
-                    currentBuild.result = 'SUCCESS'
-                    
+                    sh "AWS_DEFAULT_REGION=NYC3 AWS_ACCESS_KEY_ID=${DO_SP_ID} AWS_SECRET_ACCESS_KEY=${DO_SP_SC} aws s3 sync dist/terragon s3://www-terragon-us/ --endpoint-url https://nyc3.digitaloceanspaces.com --acl public-read"
                 }
                 
             }
