38a6624b9c276f47b4f7923c5a1627eb395be663
Mike Keen <mkeen.atl@gmail.com>
2019-06-11 14:10:20 -0400

    this is really annoying

--- jenkins/api-build-deploy.groovy
+++ jenkins/api-build-deploy.groovy
@@ -1,19 +1,18 @@
 node {
     stage 'Checkout'
-    cleanWs()
     git credentialsId: 'github_deploy_api', url: 'git@github.com:terragon-social-club/api.git'
     
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: 'xterm']) {
-        stage'NPM Install'
-        sh 'npm install'
-        
-        stage 'NPM Build'
-        sh 'npm run build-ts'
-        
-        stage 'Publish'
         withCredentials([
             string(credentialsId: 'npm_token', variable: 'N_TOKEN')
-        ]) {
+        ]) {    
+            stage name: 'NPM Install', concurrency: 1
+            sh 'npm install'
+        
+            stage name: 'NPM Build', concurrency: 1
+            sh 'npm run build-ts'
+            
+            stage name: 'Publish', concurrency: 1
             sh 'git add . && git commit -m "Jolly good."'
             sh 'npm version patch --no-git-tag-version'
             sh "echo //registry.npmjs.org/:_authToken=$N_TOKEN > .npmrc"
