656aceb10e4146262868df35003e8ffcd0eba82a
Mike Keen <mwk@mikekeen.com>
2019-12-05 23:30:30 -0500

    bam

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,8 +84,8 @@ module "CouchDB" {
 
 module "HAProxy" {
   source = "./modules/salt-minion"
-  node_count = 0
-  provision = false
+  node_count = 1
+  provision = true
   name = "haproxy"
   size = "s-2vcpu-2gb"
   domain_id = "terragon.us"
