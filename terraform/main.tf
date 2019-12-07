ea85804a36e047f255332959f43fdb33b134c443
Mike Keen <mwk@mikekeen.com>
2019-12-07 14:36:28 -0500

    deprovision

--- terraform/main.tf
+++ terraform/main.tf
@@ -208,8 +208,8 @@ resource "digitalocean_record" "couchdb_frontend" {
 
 module "NodeJSAPI" {
   source = "./modules/salt-minion"
-  node_count = 1
-  provision = true
+  node_count = 0
+  provision = false
   
   name = "nodejs-api"
   size = "s-2vcpu-2gb"
@@ -231,8 +231,8 @@ module "NodeJSAPI" {
 
 module "HAProxyNodeJSAPI" {
   source = "./modules/salt-minion"
-  node_count = 1
-  provision = true
+  node_count = 0
+  provision = false
   name = "haproxy-nodejsapi"
   size = "s-2vcpu-2gb"
   domain_id = "terragon.us"
