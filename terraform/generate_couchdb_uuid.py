fc7c88ec3798bca407fa7b9116a123cf4f734658
Mike Keen <mwk@mikekeen.com>
2019-12-21 18:07:59 -0500

    adjust

--- terraform/generate_couchdb_uuid.py
+++ terraform/generate_couchdb_uuid.py
@@ -1,5 +1,4 @@
 import sys, json, os, subprocess
-from subprocess import Popen, PIPE, STDOUT
 data = json.load(sys.stdin)
 data['uuids'] = ["dedd"]
 
@@ -11,7 +10,5 @@ subprocess.call(['chmod', '0600', 'key'])
 
 command = "ssh -i key root@" + data['master_public_ip'] + " \"ssh " + data['couch_private_ip'] + " curl -s 'http://" + data['couch_private_ip'] + ":5984/_uuids/\?count=1'\""
 
-p = Popen([command], shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-stdout_data = p.communicate(input=data['private_key'])[0]
-d = json.loads(stdout_data)
-print json.dumps(d['uuids'][0])
+d = json.loads(subprocess.check_output([command]))
+print("{\"uuid\": \"" + d['uuids'][0] + "\"}")
