b1769e205259ec647694159719aefaef16484b7c
Mike Keen <mwk@mikekeen.com>
2019-12-21 18:36:46 -0500

    john brown

--- terraform/generate_couchdb_uuid.py
+++ terraform/generate_couchdb_uuid.py
@@ -1,5 +1,5 @@
 import sys, json, os, subprocess
 data = json.load(sys.stdin)
 
-d = json.loads(subprocess.run(['ssh', '-i /dev/stdin', "root@" + data['master_public_ip'] + " \"ssh -o StrictHostKeyChecking=no " + data['couch_private_ip'] + " curl -s 'http://" + data['couch_private_ip'] + ":5984/_uuids/\?count=1'\""], check=True, input=data['private_key'], shell=True))
+d = json.loads(subprocess.check_output(['ssh', '-i /dev/stdin', "root@" + data['master_public_ip'] + " \"ssh -o StrictHostKeyChecking=no " + data['couch_private_ip'] + " curl -s 'http://" + data['couch_private_ip'] + ":5984/_uuids/\?count=1'\""], check=True, input=data['private_key'], shell=True))
 print("{\"uuid\": \"" + d['uuids'][0] + "\"}")
