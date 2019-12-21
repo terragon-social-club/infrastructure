cfb64ebaa73b9489ac5dda3e3fb591fc9e4dc450
Mike Keen <mwk@mikekeen.com>
2019-12-21 14:45:10 -0500

    os

--- terraform/generate_couchdb_uuid.py
+++ terraform/generate_couchdb_uuid.py
@@ -1,4 +1,4 @@
-import sys, json;
+import sys, json, os;
 data = json.load(sys.stdin)
 
 command = "echo " + data['private_key'] + " | ssh -q -i /dev/stdin root@" + data['master_public_ip'] + "\"ssh " + data['couch_private_ip'] + "\" 'curl -s http://'" + data['couch_private_ip'] + ":5984\?count=1"
