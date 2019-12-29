5271b42d5e0a125a78330014032057e5537a200e
Mike Keen <mwk@mikekeen.com>
2019-12-29 13:41:12 -0500

    be slow
    Signed-off-by: Mike Keen <mwk@mikekeen.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,8 +71,8 @@ module "Salt_Master" {
 
 module "Logstash" {
   source = "./modules/salt-minion"
-  node_count = 1
-  provision = true
+  node_count = 0
+  provision = false
   name = "logstash"
   size = "c-2"
   domain_id = "terragon.us"
@@ -109,8 +109,8 @@ resource "digitalocean_firewall" "beats_to_logstash" {
 
 module "ElasticSearch" {
   source = "./modules/salt-minion"
-  node_count = 1
-  provision = true
+  node_count = 0
+  provision = false
   name = "elasticsearch"
   size = "c-4"
   domain_id = "terragon.us"
