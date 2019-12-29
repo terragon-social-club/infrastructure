fdcccc2d88f4bd0df3e6afef4b8e39a51766f752
Mike Keen <mwk@mikekeen.com>
2019-12-29 18:44:43 -0500

    fix couh
    Signed-off-by: Mike Keen <mwk@mikekeen.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -290,8 +290,8 @@ module "CouchDB" {
 
 module "HAProxyCouchDB" {
   source = "./modules/salt-minion"
-  node_count = 1
-  provision = true
+  node_count = 0
+  provision = false
   name = "haproxy-couchdb"
   size = "s-2vcpu-2gb"
   domain_id = "terragon.us"
