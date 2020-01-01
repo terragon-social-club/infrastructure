bcae088ddc29d24d13989ed5f38bd2bef3ef3736
Mike Keen <mwk@mikekeen.com>
2020-01-01 11:57:22 -0500

    couchdb up
    Signed-off-by: Mike Keen <mwk@mikekeen.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -269,8 +269,8 @@ resource "random_password" "couch_pass" {
 
 module "CouchDB" {
   source = "./modules/salt-minion"
-  node_count = 0
-  provision = false
+  node_count = 3
+  provision = true
   name = "couchdb"
   size = "s-2vcpu-2gb"
   domain_id = "terragon.us"
@@ -290,8 +290,8 @@ module "CouchDB" {
 
 module "HAProxyCouchDB" {
   source = "./modules/salt-minion"
-  node_count = 0
-  provision = false
+  node_count = 1
+  provision = true
   name = "haproxy-couchdb"
   size = "s-2vcpu-2gb"
   domain_id = "terragon.us"
