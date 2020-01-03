502ea23ad70ab629e4981c0e1e5088edbc27a1fe
Mike Keen <mwk@mikekeen.com>
2020-01-03 18:13:15 -0500

    more nodes
    Signed-off-by: Mike Keen <mwk@mikekeen.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -269,7 +269,7 @@ resource "random_password" "couch_pass" {
 
 module "CouchDB" {
   source = "./modules/salt-minion"
-  node_count = 1
+  node_count = 3
   provision = true
   name = "couchdb"
   size = "s-2vcpu-2gb"
