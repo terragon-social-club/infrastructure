084e7e765f1edde0d452790d032bd7df6f820896
Mike Keen <mwk@mikekeen.com>
2020-01-07 00:24:49 -0500

    upgrade haproxy again

--- terraform/modules/couchdb/couchdb.tf
+++ terraform/modules/couchdb/couchdb.tf
@@ -49,7 +49,7 @@ module "CouchDBNode" {
   node_count = var.couchdb_replicas
   provision = var.couchdb_replicas > 0
   name = "couchdb"
-  size = "c-16"
+  size = "c-32"
   domain_id = "terragon.us"
   keys = var.ssh_keys
   
