bfb01d1202bca205c7a4e381724a6fe0f51f8299
Mike Keen <mwk@mikekeen.com>
2020-01-06 23:26:51 -0500

    haproxy upgrade

--- terraform/modules/couchdb/couchdb.tf
+++ terraform/modules/couchdb/couchdb.tf
@@ -67,7 +67,7 @@ module "HAProxy" {
   node_count = var.couchdb_proxy_online == true ? 1 : 0
   provision = var.couchdb_proxy_online
   name = "haproxy-couchdb"
-  size = "s-1vcpu-1gb"
+  size = "c-2"
   domain_id = "terragon.us"
   custom_fqdn = "couchdb"
   keys = var.ssh_keys
