8a6ebcd608ccfcbd9d3972cc9a9e3cc7f94b3612
Mike Keen <mwk@mikekeen.com>
2020-02-16 12:46:12 -0500

    lets start over with couch

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ variable "cluster_makeup" {
       proxy_size = "s-1vcpu-1gb"
       proxy_provisioned = true
       haproxy_domain = "couchdb"
-      node_count = 3
+      node_count = 0
     }
 
     api = {
